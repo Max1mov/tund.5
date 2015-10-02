33e0b4349036e5b5785f27e40ed6ac760ee4d88f
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-10-02 11:17:21 +0300

    added files, function

--- functions.php
+++ functions.php
@@ -1,20 +1,57 @@
 <?php
 
 
+	require_once("../configGLOBAL.php");
+	$database = "if15_vitamak";
+	$mysqli = new mysqli($servername, $username, $password, $database);
+	 // paneme ühenduse kinni
+	$mysqli->close();
 
-	function welcomeUser($name, $age){
-		echo "Tere ".$name.", age is ".$age."<br>";
+	
+	//lisamine kasutaja abˇi
+	function createUser(){
+		
+		$mysqli = new mysqli($servername, $username, $password, $database);
+		$password_hash = hash("sha512", $create_password);
+				echo "<br>";
+				echo $password_hash;
+				
+				$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+				
+				//echo $mysqli->error;
+				//echo $stmt->error;
+				//asendame ? märgid muutujate väärtuste
+				// ss - s tähendab string iga muutuja kohta
+				$stmt->bind_param("ss", $create_email, $password_hash);
+				$stmt->execute();
+				$stmt->close();
+			$mysqli->close();
+			
+		}
+
+
+
+
+	function LoginUser(){
+		$mysqli = new mysqli($servername, $username, $password, $database);
+				$password_hash = hash("sha512", $password);
+				
+				$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+				$stmt->bind_param("ss", $email, $password_hash);
+				
+				//paneme vastuse muutujatesse
+				$stmt->bind_result($id_from_db, $email_from_db);
+				$stmt->execute();
+				
+				//küsima kas AB'ist saime kätte
+				if($stmt->fetch()){
+					//leidis
+					echo "kasutaja id=".$id_from_db;
+				}else{
+					// tühi, ei leidnud , ju siis midagi valesti
+					echo "Wrong password or email!";
+					$mysqli->close();
 	}
-	
-	$first_name = "Juku";
-	welcomeUser($first_name, 88);
-	
-	$first_name = "Juhan";
-	welcomeUser($first_name, 44);
-	
-	welcomeUser("vitali", 4);
-	
-	
-	
+	}
 	
 ?>
