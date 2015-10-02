7f1b0b9ea58a29ce0c9b1099ac0135983bb5c233
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-10-02 10:52:23 +0300

    added

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+	function welcomeUser($name, $age){
+		echo "Tere ".$name.", age is ".$age."<br>";
+	}
+	
+	$first_name = "Juku";
+	welcomeUser($first_name, 88);
+	
+	$first_name = "Juhan";
+	welcomeUser($first_name, 44);
+	
+	welcomeUser("vitali", 4);
+	
+	
+	
+	
+?>
